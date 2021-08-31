781db0755d4915a62d73dd57c102b7632e76e6c1
Juan José Osorio <j.osorioc@uniandes.edu.co>
2021-08-30 23:46:45 -0500

    Carga de los ingredientes

--- Taller 1/src/consola/Aplicacion.java
+++ Taller 1/src/consola/Aplicacion.java
@@ -1,11 +1,25 @@
 package consola;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import modelo.Ingrediente;
+import modelo.Restaurante;
+
 public class Aplicacion
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		// TODO Auto-generated method stub
+		Restaurante startRestaurante = new Restaurante();
+		
+		String nuestroDirectory = System.getProperty("user.dir");
+		
+		// Carga de la información.
+		startRestaurante.cargarInformacionRestaurante(new File(nuestroDirectory + "/data/ingredientes.txt"), null, null);
+		
 
 	}
 	
--- Taller 1/src/modelo/Restaurante.java
+++ Taller 1/src/modelo/Restaurante.java
@@ -1,10 +1,25 @@
 package modelo;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import java.io.BufferedReader;
 
 public class Restaurante
 {
+	// Atributos
+	private ArrayList<Ingrediente> ingredientes = new ArrayList<>();
+	private ArrayList<Producto> productos = new ArrayList<>();
+	
+	private Map<String, ArrayList<Producto>> combosMap = new HashMap<>();
+	
+	
 	// Constructor
 	public Restaurante()
 	{
@@ -33,25 +48,46 @@ public class Restaurante
 	
 	public ArrayList<Producto> getMenuBase()
 	{
-		return null; //TODO
+		return this.productos;
 	}
 	
 	
 	public ArrayList<Ingrediente> getIngredientes()
 	{
-		return null; //TODO
+		return this.ingredientes;
 	}
 	
 	
-	public void cargarInformacionRestaurante(File archivoIngredientes, File archivoMenu, File archivoCombos)
+	public void cargarInformacionRestaurante(File archivoIngredientes, File archivoMenu, File archivoCombos) throws IOException
 	{
-		//TODO
+		cargarIngredientes(archivoIngredientes);
 	}
 	
 	
-	private void cargarIngredientes(File archivoIngredientes)
+	private void cargarIngredientes(File archivoIngredientes) throws IOException 
 	{
-		//TODO
+		//ingredientes.txt
+		archivoIngredientes.createNewFile();
+		Reader targetReader = new FileReader(archivoIngredientes);
+		
+		BufferedReader br = new BufferedReader(targetReader);
+		
+		String linea = br.readLine();
+		
+		while (linea != null)
+		{
+			// Separar los valores que estaban en una line.
+			String[] parteStrings = linea.split(";");
+			
+			String nombreIngrediente = parteStrings[0];
+			int precioIngrediente = Integer.parseInt(parteStrings[1]);
+			
+			Ingrediente nuevoIngrediente = new Ingrediente(nombreIngrediente, precioIngrediente);
+			
+			this.ingredientes.add(nuevoIngrediente);
+			linea = br.readLine();
+		}
+		
 	}
 	
 	
